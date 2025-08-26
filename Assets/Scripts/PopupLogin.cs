f3a48cade112af4b930f7f6e480d6dafd2ba4c3b
Kseongmin1107 <ksmsky1107@gmail.com>
2025-08-26 12:30:17 +0900

    Feat_PopupError

--- Assets/Scripts/PopupLogin.cs
+++ Assets/Scripts/PopupLogin.cs
@@ -10,6 +10,9 @@ public class PopupLogin : MonoBehaviour
     public GameObject mainUI;
     public GameObject signUpUI;
 
+    public GameObject errorPopupUI;
+    public TextMeshProUGUI errorText;
+
     public void OnLoginButtonClicked()
     {
         string inputID = idInputField.text;
@@ -19,6 +22,12 @@ public class PopupLogin : MonoBehaviour
 
         UserData savedData = GameManager.Instance.userData;
 
+        if (string.IsNullOrEmpty(inputID) || string.IsNullOrEmpty(inputPassword))
+        {
+            ShowErrorPopup("아이디와 비밀번호를 입력해주세요.");
+            return;
+        }
+
         if (inputID == savedData.id && inputPassword == savedData.password)
         {
             Debug.Log("로그인 성공!");
@@ -27,7 +36,7 @@ public class PopupLogin : MonoBehaviour
         }
         else
         {
-            Debug.Log("로그인 실패: 아이디 또는 비밀번호가 올바르지 않습니다.");
+            ShowErrorPopup("아이디 또는 비밀번호가 올바르지 않습니다.");
         }
     }
 
@@ -36,5 +45,20 @@ public class PopupLogin : MonoBehaviour
         signUpUI.SetActive(true);
         gameObject.SetActive(false);
         Debug.Log("회원가입 버튼 클릭됨");
+    }
+    private void ShowErrorPopup(string message)
+    {
+        if (errorPopupUI != null)
+        {
+            errorText.text = message;
+            errorPopupUI.SetActive(true);
+        }
+    }
+    public void CloseErrorPopup()
+    {
+        if (errorPopupUI != null)
+        {
+            errorPopupUI.SetActive(false);
+        }
     }
 }
